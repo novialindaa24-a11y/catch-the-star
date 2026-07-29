0a7c286abe929ae9fa855f030712fe62cc833d05
novialindaa24-a11y <novialindaa24@gmail.com>
2026-07-29 08:32:43 +0700

    Update playercontroller.css

--- playercontroller.cs
+++ playercontroller.cs
@@ -1,1 +1,14 @@
+using UnityEngine;
 
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(x, y, 0) * speed * Time.deltaTime);
+    }
+}
